6a46e5586e372493371b4c6c6bf985385e4b2f8c
Nicolas Mesa <nicolasmesa@gmail.com>
2019-06-18 22:01:54 -0700

    Adding UART capabilities

--- src/kernel.c
+++ src/kernel.c
@@ -1,16 +1,14 @@
- #include "mini_uart.h"
-
- #define BAUD_RATE (115200)
+ #include "uart.h"
 
  void kernel_main(void) {
-     mini_uart_init(BAUD_RATE);
-     mini_uart_send_string("Hello world!\r\n");
+     uart_init();
+     uart_send_string("Hello world!\r\n");
 
      for(int i = 0; i < 1000; i++) {
-       mini_uart_send_string("Hello world!\r\n");
+       uart_send_string("Hello world!\r\n");
      }
 
      while (1) {
-         mini_uart_send(mini_uart_recv());
+         uart_send(uart_recv());
      }
  }
