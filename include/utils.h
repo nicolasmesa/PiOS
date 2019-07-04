72f92f1c5d371cde8b59f2625e165a2ab485f367
Nicolas Mesa <nicolasmesa@gmail.com>
2019-07-04 14:09:24 -0700

    Adding get_cpuid function and some reformatting

--- include/utils.h
+++ include/utils.h
@@ -5,5 +5,6 @@ extern void delay ( unsigned long);
 extern void put32 ( unsigned long, unsigned int );
 extern unsigned int get32 ( unsigned long );
 extern void branch_to_address( void * );
+extern unsigned int get_cpuid();
 
 #endif  /*_BOOT_H */
