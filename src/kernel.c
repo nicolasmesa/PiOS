6b6c72713ac1fdea86f0457c3802f2b431864cc4
Nicolas Mesa <nicolasmesa@gmail.com>
2019-07-11 21:58:00 -0700

    Making multiple processors work

--- src/kernel.c
+++ src/kernel.c
@@ -1,6 +1,19 @@
 #include "uart.h"
 #include "utils.h"
 
+#define BUFF_SIZE 100
+// The magic address is an arbitrary memory address that we use
+// to let other processors know what to do. There are two scenarios:
+// 1. We're going to copy the kernel. In this scenario, we want the CPU
+//   to jump to address 0x00 when the kernel has been copied. This will
+//   ensure that the CPU starts executing the new kernel.
+// 2. We're not going to copy the kernel, or the kernel has been copied already.
+//   In this case, we simply allow the CPUs to continue their execution.
+// This address can have values 0, 1, 2. CPUs 1-3 spin while this address is
+// equal to 0. Once it changes value, 1 represents the first scenario described
+// above and 2 represents the second scenario.
+#define MAGIC_ADDRESS ((char *)0xD000)
+
 void hang() {
     while (1)
         ;
@@ -56,6 +69,7 @@ void copy_and_jump_to_kernel() {
 
     uart_send_string("Done copying kernel\r\n");
 
+    *MAGIC_ADDRESS = 1;
     branch_to_address((void *)0x00);
 }
 
@@ -95,24 +109,55 @@ void copy_current_kernel_and_jump(char *new_address) {
     call_function();
 }
 
-#define BUFF_SIZE 100
+int init_step = 0;
 
 void kernel_main(void) {
-    uart_init();
+    int cpuid = get_cpuid();
 
-    char buffer[BUFF_SIZE];
-
-    readline(buffer, BUFF_SIZE);
-    if (strcmp(buffer, "kernel") == 0) {
-        copy_current_kernel_and_jump((char *)0x8000);
+    if (cpuid == 0) {
+        uart_init();
+        *MAGIC_ADDRESS = 0;
+        init_step = 1;
     }
 
-    int cpuid = get_cpuid();
+    // Required to avoid race conditions with MAGIC_ADDRESS. We only continue
+    // when we're certain that the value of MAGIC_ADDRESS has been set to 0.
+    while (init_step == 0)
+        ;
+
+    char buffer[BUFF_SIZE];
+    if (cpuid == 0) {
+        readline(buffer, BUFF_SIZE);
+
+        if (strcmp(buffer, "kernel") == 0) {
+            copy_current_kernel_and_jump((char *)0x8000);
+        }
+        *MAGIC_ADDRESS = 2;
+    }
+
+    while (*MAGIC_ADDRESS == 0)
+        ;
+
+    if (*MAGIC_ADDRESS == 1) {
+        branch_to_address((void *)0x00);  // never returns;
+    }
+
+    while (init_step < (cpuid + 1))
+        ;
+
+    delay(100000);
     uart_send_string("Hello from CPU ");
     uart_send(cpuid + '0');
     uart_send_string("\r\n");
+    delay(100000);
+    init_step++;
 
-    while (1) {
-        uart_send(uart_recv());
+    if (cpuid == 0) {
+        while (1) {
+            uart_send(uart_recv());
+        }
+    } else {
+        while (1)
+            ;
     }
 }
