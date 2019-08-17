925ed3915172fe0d0ac45a7acc360ce4f4fa998c
Nicolas Mesa <nicolasmesa@gmail.com>
2019-08-17 14:07:27 -0700

    Multiprocessor kernel working (fixed)

--- src/kernel.c
+++ src/kernel.c
@@ -3,15 +3,21 @@
 
 #define BUFF_SIZE 100
 // The magic address is an arbitrary memory address that we use
-// to let other processors know what to do. There are two scenarios:
-// 1. We're going to copy the kernel. In this scenario, we want the CPU
-//   to jump to address 0x00 when the kernel has been copied. This will
-//   ensure that the CPU starts executing the new kernel.
-// 2. We're not going to copy the kernel, or the kernel has been copied already.
-//   In this case, we simply allow the CPUs to continue their execution.
-// This address can have values 0, 1, 2. CPUs 1-3 spin while this address is
-// equal to 0. Once it changes value, 1 represents the first scenario described
-// above and 2 represents the second scenario.
+// to let other processors know what to do. There are four states (or values):
+// 0. We initialize this to 0 while we decide if we're going to copy a new
+//  kernel or if we cant to keep going in the current kernel.
+// 1. We just finished copying the current kernel over to the new address and
+//  other CPUs should jump to start executing code in that newly copied kernel
+//  while CPU 1 copies the new kernel (overwriting the old one).
+// 2. We're done copying the new kernel so the CPUs should jump back to address
+//  0 to start executing the new kernel. This address can have values 0, 1, 2.
+// 3. We're not copying a new kernel, so continue with the execution of the
+// current kernel.
+// TLDR
+// 0 -- Loading
+// 1 -- Copied current kernel to new place (jump there).
+// 2 -- Copied new kernel to new place. (jump to 0).
+// 3 -- Not going to copy anything, keep going.
 #define MAGIC_ADDRESS ((char *)0xD000)
 
 void hang() {
@@ -21,12 +27,8 @@ void hang() {
 
 int strcmp(char *str1, char *str2) {
     while (1) {
-        if (*str1 < *str2) {
-            return -1;
-        }
-
-        if (*str1 > *str2) {
-            return 1;
+        if (*str1 != *str2) {
+            return *str1 - *str2;
         }
 
         if (*str1 == '\0') {
@@ -75,7 +77,7 @@ void copy_and_jump_to_kernel() {
 
     // Set this to one (see scenario 1 above) to make other CPUs jump to this
     // address too.
-    *MAGIC_ADDRESS = 1;
+    *MAGIC_ADDRESS = 2;
     branch_to_address((void *)0x00);
 }
 
@@ -104,6 +106,10 @@ void copy_current_kernel_and_jump(char *new_address) {
     // Cast the function pointer to char* to deal with bytes.
     char *original_function_address = (char *)&copy_and_jump_to_kernel;
 
+    // Allow the other CPUs to jump to the copied section
+    *MAGIC_ADDRESS = 1;
+    delay(10000);
+
     // Add the new address (we're assuming that the original kernel resides in
     // address 0). copied_function_address should now contain the address of the
     // original function but in the new location.
@@ -115,9 +121,54 @@ void copy_current_kernel_and_jump(char *new_address) {
     call_function();
 }
 
+void handle_cpu_and_jump() {
+    while (*MAGIC_ADDRESS == 1)
+        ;
+
+    branch_to_address((void *)0x00);  // never returns;
+}
+
 int init_step = 0;
 
+// 0 -- Loading
+// 1 -- Copied current kernel to new place (jump there)
+// 2 -- Copied new kernel to new place. (jump to 0)
+// 3 -- Not goint to copy anything, keep going.
+void handle_cpu(int cpuid, char *new_address) {
+    if (cpuid == 0) {
+        return;
+    }
+
+    // Required to avoid race conditions with MAGIC_ADDRESS. We only continue
+    // when we're certain that the value of MAGIC_ADDRESS has been set to 0.
+    while (init_step == 0)
+        ;
+
+    while (*MAGIC_ADDRESS == 0)
+        ;
+
+    // We're not going to copy the kernel so we'll return
+    if (*MAGIC_ADDRESS == 3) {
+        return;
+    }
+
+    char *original_function_address = (char *)handle_cpu_and_jump;
+
+    // Add the new address (we're assuming that the original kernel resides in
+    // address 0). copied_function_address should now contain the address of the
+    // original function but in the new location.
+    char *copied_function_address =
+        original_function_address + (long)new_address;
+
+    // Cast the address back to a function and call it.
+    void (*call_function)() = (void (*)())copied_function_address;
+    call_function();
+
+    branch_to_address((void *)0x00);  // never returns;
+}
+
 void kernel_main(void) {
+    char *new_address = (char *)0x8000;
     int cpuid = get_cpuid();
 
     if (cpuid == 0) {
@@ -126,31 +177,16 @@ void kernel_main(void) {
         init_step = 1;
     }
 
-    // Required to avoid race conditions with MAGIC_ADDRESS. We only continue
-    // when we're certain that the value of MAGIC_ADDRESS has been set to 0.
-    while (init_step == 0)
-        ;
+    handle_cpu(cpuid, new_address);
 
     char buffer[BUFF_SIZE];
     if (cpuid == 0) {
         readline(buffer, BUFF_SIZE);
 
         if (strcmp(buffer, "kernel") == 0) {
-            copy_current_kernel_and_jump((char *)0x8000);
+            copy_current_kernel_and_jump(new_address);
         }
-        *MAGIC_ADDRESS = 2;
-    }
-
-    while (*MAGIC_ADDRESS == 0)
-        ;
-
-    // If true, we're in scenario 1 (mentioned above). CPU 0 finished copying
-    // the kernel so we can safely jump to address 0x00 to start executing in
-    // the new kernel context.
-    // If not true, MAGIC_ADDRESS should be equal to 2 which means that the CPUs
-    // should continue with the execution.
-    if (*MAGIC_ADDRESS == 1) {
-        branch_to_address((void *)0x00);  // never returns;
+        *MAGIC_ADDRESS = 3;
     }
 
     // Each CPU will increment init_step when it's done sending the string
