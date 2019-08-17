6c151e5a99f50de095473133883aac2248fb5cc0
Nicolas Mesa <nicolasmesa@gmail.com>
2019-08-17 14:32:26 -0700

    Removing most of the SMP support

--- src/kernel.c
+++ src/kernel.c
@@ -2,23 +2,6 @@
 #include "utils.h"
 
 #define BUFF_SIZE 100
-// The magic address is an arbitrary memory address that we use
-// to let other processors know what to do. There are four states (or values):
-// 0. We initialize this to 0 while we decide if we're going to copy a new
-//  kernel or if we cant to keep going in the current kernel.
-// 1. We just finished copying the current kernel over to the new address and
-//  other CPUs should jump to start executing code in that newly copied kernel
-//  while CPU 1 copies the new kernel (overwriting the old one).
-// 2. We're done copying the new kernel so the CPUs should jump back to address
-//  0 to start executing the new kernel. This address can have values 0, 1, 2.
-// 3. We're not copying a new kernel, so continue with the execution of the
-// current kernel.
-// TLDR
-// 0 -- Loading
-// 1 -- Copied current kernel to new place (jump there).
-// 2 -- Copied new kernel to new place. (jump to 0).
-// 3 -- Not going to copy anything, keep going.
-#define MAGIC_ADDRESS ((char *)0xD000)
 
 void hang() {
     while (1)
@@ -74,10 +57,6 @@ void copy_and_jump_to_kernel() {
     uart_send_int(checksum);
 
     uart_send_string("Done copying kernel\r\n");
-
-    // Set this to one (see scenario 1 above) to make other CPUs jump to this
-    // address too.
-    *MAGIC_ADDRESS = 2;
     branch_to_address((void *)0x00);
 }
 
@@ -106,10 +85,6 @@ void copy_current_kernel_and_jump(char *new_address) {
     // Cast the function pointer to char* to deal with bytes.
     char *original_function_address = (char *)&copy_and_jump_to_kernel;
 
-    // Allow the other CPUs to jump to the copied section
-    *MAGIC_ADDRESS = 1;
-    delay(10000);
-
     // Add the new address (we're assuming that the original kernel resides in
     // address 0). copied_function_address should now contain the address of the
     // original function but in the new location.
@@ -119,93 +94,31 @@ void copy_current_kernel_and_jump(char *new_address) {
     // Cast the address back to a function and call it.
     void (*call_function)() = (void (*)())copied_function_address;
     call_function();
-}
-
-void handle_cpu_and_jump() {
-    while (*MAGIC_ADDRESS == 1)
-        ;
-
-    branch_to_address((void *)0x00);  // never returns;
 }
 
 int init_step = 0;
-
-// 0 -- Loading
-// 1 -- Copied current kernel to new place (jump there)
-// 2 -- Copied new kernel to new place. (jump to 0)
-// 3 -- Not goint to copy anything, keep going.
-void handle_cpu(int cpuid, char *new_address) {
-    if (cpuid == 0) {
-        return;
-    }
-
-    // Required to avoid race conditions with MAGIC_ADDRESS. We only continue
-    // when we're certain that the value of MAGIC_ADDRESS has been set to 0.
-    while (init_step == 0)
-        ;
-
-    while (*MAGIC_ADDRESS == 0)
-        ;
-
-    // We're not going to copy the kernel so we'll return
-    if (*MAGIC_ADDRESS == 3) {
-        return;
-    }
-
-    char *original_function_address = (char *)handle_cpu_and_jump;
-
-    // Add the new address (we're assuming that the original kernel resides in
-    // address 0). copied_function_address should now contain the address of the
-    // original function but in the new location.
-    char *copied_function_address =
-        original_function_address + (long)new_address;
-
-    // Cast the address back to a function and call it.
-    void (*call_function)() = (void (*)())copied_function_address;
-    call_function();
-
-    branch_to_address((void *)0x00);  // never returns;
-}
 
 void kernel_main(void) {
     char *new_address = (char *)0x8000;
     int cpuid = get_cpuid();
 
-    if (cpuid == 0) {
-        uart_init();
-        *MAGIC_ADDRESS = 0;
-        init_step = 1;
+    if (cpuid != 0) {
+        hang();
     }
 
-    handle_cpu(cpuid, new_address);
+    uart_init();
 
     char buffer[BUFF_SIZE];
-    if (cpuid == 0) {
-        readline(buffer, BUFF_SIZE);
+    readline(buffer, BUFF_SIZE);
 
-        if (strcmp(buffer, "kernel") == 0) {
-            copy_current_kernel_and_jump(new_address);
-        }
-        *MAGIC_ADDRESS = 3;
+    if (strcmp(buffer, "kernel") == 0) {
+        copy_current_kernel_and_jump(new_address);
     }
-
-    // Each CPU will increment init_step when it's done sending the string
-    // We use this to make sure only one CPU is sending at a time.
-    while (init_step < (cpuid + 1))
-        ;
-
     uart_send_string("Hello from CPU ");
     uart_send(cpuid + '0');
     uart_send_string("\r\n");
-    init_step++;
 
-    if (cpuid == 0) {
-        while (1) {
-            uart_send(uart_recv());
-        }
-    } else {
-        // hang other CPUs
-        while (1)
-            ;
+    while (1) {
+        uart_send(uart_recv());
     }
 }
