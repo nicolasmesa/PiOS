767d1501b8e3942272f10c013ea589fe7a3bb162
Nicolas Mesa <nicolasmesa@gmail.com>
2019-08-24 12:51:49 -0700

    Implementing the boot_send protocol in the kernel side

--- include/uart.h
+++ include/uart.h
@@ -1,9 +1,11 @@
-#ifndef	_UART_H
-#define	_UART_H
+#ifndef _UART_H
+#define _UART_H
 
 void uart_init();
 void uart_send_string(char *str);
 void uart_send(char c);
 char uart_recv();
+void uart_send_int(int number);
+int uart_read_int();
 
-#endif  /*_UART_H */
+#endif /*_UART_H */
